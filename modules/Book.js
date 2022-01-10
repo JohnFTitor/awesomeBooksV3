95c4f25a8b1028b009e2c71abb13df9b6a4bae53
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-01-10 15:30:16 -0600

    Fix indicator not defined in AddBook.js

--- modules/Book.js
+++ modules/Book.js
@@ -1,51 +1,50 @@
 const booksContainer = document.querySelector('.book-container');
 
-
 export default class Book {
     static collection = [];
 
     constructor(title, author) {
-        this.title = title;
-        this.author = author;
+      this.title = title;
+      this.author = author;
     }
 
     static saveDataLocally = () => {
-        const stringifiedLibrary = JSON.stringify(Book.collection);
-        localStorage.setItem('library', stringifiedLibrary);
+      const stringifiedLibrary = JSON.stringify(Book.collection);
+      localStorage.setItem('library', stringifiedLibrary);
     }
 
     removeBook = () => {
-        Book.collection = Book.collection.filter((book) => book.title !== this.title
+      Book.collection = Book.collection.filter((book) => book.title !== this.title
             || book.author !== this.author);
-        Book.saveDataLocally();
+      Book.saveDataLocally();
     };
 
     addBook = () => {
-        Book.collection.push(this);
-        Book.saveDataLocally();
-        this.displayBook();
+      Book.collection.push(this);
+      Book.saveDataLocally();
+      this.displayBook();
     }
 
     displayBook = () => {
-        const bookCard = document.createElement('div');
-        const cardTitle = document.createElement('p');
-        const removeButton = document.createElement('button');
+      const bookCard = document.createElement('div');
+      const cardTitle = document.createElement('p');
+      const removeButton = document.createElement('button');
 
-        bookCard.classList.add('book-Card');
+      bookCard.classList.add('book-Card');
 
-        cardTitle.textContent = `"${this.title}"   by   ${this.author}`;
-        removeButton.textContent = 'Remove';
-        removeButton.classList.add('remove');
+      cardTitle.textContent = `"${this.title}"   by   ${this.author}`;
+      removeButton.textContent = 'Remove';
+      removeButton.classList.add('remove');
 
-        bookCard.appendChild(cardTitle);
-        bookCard.appendChild(removeButton);
+      bookCard.appendChild(cardTitle);
+      bookCard.appendChild(removeButton);
 
-        booksContainer.appendChild(bookCard);
+      booksContainer.appendChild(bookCard);
 
-        removeButton.addEventListener('click', () => {
-            this.removeBook();
-            Book.saveDataLocally();
-            booksContainer.removeChild(bookCard);
-        });
+      removeButton.addEventListener('click', () => {
+        this.removeBook();
+        Book.saveDataLocally();
+        booksContainer.removeChild(bookCard);
+      });
     }
 }
